bf1f1590eab124476e9f282419bec3a51cce1131
曹珂俭 <1849201815@qq.com>
2023-04-04 15:33:21 +0800

    重构，让create validation code 简洁

--- app/controllers/api/v1/validation_codes_controller.rb
+++ app/controllers/api/v1/validation_codes_controller.rb
@@ -1,11 +1,10 @@
 class Api::V1::ValidationCodesController < ApplicationController
   def create
-    code = SecureRandom.random_number.to_s[2..7]
-    validation_code = ValidationCode.new email: params[:email], kind: 'sign_in', code: code
+    validation_code = ValidationCode.new email: params[:email], kind: "sign_in", code: code
     if validation_code.save
-      render  status: 200
+      render status: 200
     else
-      render json: {errors: validation_code.errors}, status: 400
+      render json: { errors: validation_code.errors }, status: 400
     end
   end
 end
