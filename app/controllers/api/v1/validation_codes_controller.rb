5e54a5af1eed994c9d3d714662f3b42b015af912
曹珂俭 <1849201815@qq.com>
2023-04-04 15:45:54 +0800

    发送验证码太频繁会返回429

--- app/controllers/api/v1/validation_codes_controller.rb
+++ app/controllers/api/v1/validation_codes_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::ValidationCodesController < ApplicationController
   def create
-    validation_code = ValidationCode.new email: params[:email], kind: "sign_in", code: code
+    if ValidationCode.exists?(email: params[:email], kind:'sign_in',created_at: 1.minute.ago..Time.now)
+      render status: :too_many_requests
+      return
+    end
+    validation_code = ValidationCode.new email: params[:email], kind: "sign_in"
     if validation_code.save
       render status: 200
     else
--- spec/acceptance/validation_codes_spec.rb
+++ spec/acceptance/validation_codes_spec.rb
@@ -6,9 +6,12 @@ resource "验证码" do
     parameter :email, type: :string
     let(:email) { '1@qq.com' }
     example "请求发送验证码" do
+      expect(UserMailer).to receive(:welcome_email).with(email)
       do_request
       expect(status).to eq 200
       expect(response_body).to eq ' '
+      do_request
+      expect(status).to eq 429
     end
   end
 end
