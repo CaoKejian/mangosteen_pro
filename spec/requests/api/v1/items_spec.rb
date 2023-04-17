b117411d677d8758df1cc8e47f77f6a66eb5d792
曹珂俭 <1849201815@qq.com>
2023-04-18 07:45:36 +0800

    将tiem#index的筛选条件由created_at改为happen_at

--- spec/requests/api/v1/items_spec.rb
+++ spec/requests/api/v1/items_spec.rb
@@ -24,11 +24,11 @@ RSpec.describe "Items", type: :request do
     end
     it "按时间筛选" do
       user1 = create :user
-      item1 = create :item, created_at: "2018-01-02", user: user1
-      item2 = create :item, created_at: "2018-01-02", user: user1
-      item3 = create :item, created_at: "2019-01-01", user: user1
+      item1 = create :item, happen_at: "2018-01-02", user: user1
+      item2 = create :item, happen_at: "2018-01-02", user: user1
+      item3 = create :item, happen_at: "2019-01-01", user: user1
 
-      get "/api/v1/items?created_after=2018-01-01&created_before=2018-01-03",
+      get "/api/v1/items?happen_after=2018-01-01&happen_before=2018-01-03",
         headers: user1.generate_auth_header
       expect(response).to have_http_status 200
       json = JSON.parse(response.body)
@@ -46,9 +46,9 @@ RSpec.describe "Items", type: :request do
       expect(json["resources"][0]["id"]).to eq item1.id
     end
     it "按时间筛选（边界条件2）" do
-      item1 = create :item, created_at: "2018-01-01"
-      item2 = create :item, created_at: "2017-01-01", user: item1.user
-      get "/api/v1/items?created_after=2018-01-01",
+      item1 = create :item, happen_at: "2018-01-01"
+      item2 = create :item, happen_at: "2017-01-01", user: item1.user
+      get "/api/v1/items?happen_after=2018-01-01",
         headers: item1.user.generate_auth_header
       expect(response).to have_http_status 200
       json = JSON.parse(response.body)
@@ -57,10 +57,10 @@ RSpec.describe "Items", type: :request do
     end
     it "按时间筛选（边界条件3）" do
       user1 = create :user
-      item1 = create :item, created_at: "2018-01-01", user: user1
-      item2 = create :item, created_at: "2019-01-01", user: user1
+      item1 = create :item, happen_at: "2018-01-01", user: user1
+      item2 = create :item, happen_at: "2019-01-01", user: user1
 
-      get "/api/v1/items?created_before=2018-01-02",
+      get "/api/v1/items?happen_before=2018-01-02",
         headers: user1.generate_auth_header
       expect(response).to have_http_status 200
       json = JSON.parse(response.body)
