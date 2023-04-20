fff416f0710525140bee85ad856839acc44cb62b
曹珂俭 <1849201815@qq.com>
2023-04-21 00:09:12 +0800

    CORS支持任意域名，但不是用*来做

--- bin/setup_remote.sh
+++ bin/setup_remote.sh
@@ -76,23 +76,12 @@ cd /home/$user/deploys/$version
 mkdir ./dist
 tar xf dist.tar.gz --directory=./dist
 cd -
-# - PUID=1000
-#       - PGID=1000
-#       - TZ=Etc/UTC
-#       - URL=yourdomain.url
-#       - VALIDATION=http
-docker run -d -p 80:80 -p 443:443\
+docker run -d -p 8080:80 \
            --network=network1 \
-           -e PUID=1000 \
-           -e PGID=1000 \
-           -e TZ=Etc/GMT-8 \
-           -e URL=mangosteen2.hunger-valley.com \
-           -e VALIDATION=http \
            --name=$nginx_container_name \
-           -v /home/$user/deploys/$version/nginx-config:/config \
-           -v /home/$user/deploys/$version/nginx.default.conf:/config/nginx/site-confs/default \
+           -v /home/$user/deploys/$version/nginx.default.conf:/etc/nginx/conf.d/default.conf \
            -v /home/$user/deploys/$version/dist:/usr/share/nginx/html \
            -v /home/$user/deploys/$version/api:/usr/share/nginx/html/apidoc \
-           linuxserver/swag:latest
+           nginx:latest
 
 title '全部执行完毕'
